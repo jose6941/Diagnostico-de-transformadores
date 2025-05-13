155b75db121de8431c73867a93846ed4f79359bc
Jose6941 <jose.souza1@aluno.ifsp.edu.br>
2025-05-13 09:32:27 -0300

    .

--- Analise/RandomForest.py
+++ Analise/RandomForest.py
@@ -11,39 +11,31 @@ from sklearn.metrics import (
 )
 from sklearn.preprocessing import StandardScaler, LabelEncoder, label_binarize
 
-# Corrigindo o caminho
 input_file = 'Dataset_sintetico/dados_sinteticos_normalized.csv'
 names = ['H2', 'CH4', 'C2H2', 'C2H4', 'C2H6', 'defeito']
 features = ['H2', 'CH4', 'C2H2', 'C2H4', 'C2H6']
 target = 'defeito'
 
-# Carregar os dados
 df = pd.read_csv(input_file, names=names)
 
 X = df[features].values
 y = df[target].values
 
-# Label Encoding
 label_encoder = LabelEncoder()
 y = label_encoder.fit_transform(y)
 class_names = label_encoder.classes_
 
-# Escalonamento
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-# Divisão treino/teste
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
-# Modelo Random Forest
 rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
 rf_model.fit(X_train, y_train)
 
-# Previsões
 y_pred = rf_model.predict(X_test)
 y_pred_proba = rf_model.predict_proba(X_test)
 
-# Métricas
 accuracy = accuracy_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred, average='macro')
 kappa = cohen_kappa_score(y_test, y_pred)
@@ -57,7 +49,6 @@ print(f"Log Loss: {loss:.4f}")
 print("\nRelatório de Classificação:")
 print(classification_report(y_test, y_pred, target_names=[str(cls) for cls in label_encoder.classes_]))
 
-# Matriz de Confusão
 cm = confusion_matrix(y_test, y_pred)
 plt.figure(figsize=(8, 6))
 sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
@@ -68,7 +59,6 @@ plt.ylabel("Real")
 plt.tight_layout()
 plt.show()
 
-# Importância das Variáveis
 feature_importances = rf_model.feature_importances_
 plt.figure(figsize=(10, 6))
 plt.barh(features, feature_importances, color='skyblue')
@@ -78,7 +68,6 @@ plt.ylabel("Características")
 plt.tight_layout()
 plt.show()
 
-# Curvas ROC/AUC
 y_test_bin = label_binarize(y_test, classes=np.unique(y))
 n_classes = y_test_bin.shape[1]
 
