7026dd2fb5c19c2e60a624c796dba30be5af2c2c
Jose6941 <jose.souza1@aluno.ifsp.edu.br>
2025-06-08 16:26:46 -0300

    .

--- Diagnostico/src/components/Features/Features.jsx
+++ Diagnostico/src/components/Features/Features.jsx
@@ -11,20 +11,27 @@ const Features = () => {
           <span className="h2 features__text-gradient">Projeto</span>
         </h2>
         <div className="divRow">
-          <p className="text-reg features__subheading">
-            A técnica mais difundida para o diagnóstico dessas falhas é a Análise de Gases Dissolvidos (DGA), 
-            que interpreta os gases produzidos pela decomposição do óleo isolanteAtravés dos gases dissolvidos 
-            no óleo isolante do transformador, é possível avaliar o estado do seu isolamento com a indicação de 
-            possíveis estados de falhas que possam estar ocorrendo.
-          </p>
-          <p className="text-reg features__subheading">
-            Esses processos de análise de gases dissolvidos 
-            ou análise cromatográfica do óleo isolante são realizados a partir da amostra do óleo isolante de um 
-            transformador em operação, quantificando a concentração de certos gases gerados no equipamento e 
-            dissolvidos no óleo isolante, indicando a presença de falhas incipientes associadas à parte ativa do 
-            transformador.
-          </p>
+          <div>
+            <h2 className="h2 features__heading">Análise</h2>
+            <p className="text-reg features__subheading">
+              A técnica mais difundida para o diagnóstico dessas falhas é a Análise de Gases Dissolvidos (DGA), 
+              que interpreta os gases produzidos pela decomposição do óleo isolanteAtravés dos gases dissolvidos 
+              no óleo isolante do transformador, é possível avaliar o estado do seu isolamento com a indicação de 
+              possíveis estados de falhas que possam estar ocorrendo.
+            </p>
+          </div>
+          <div>
+            <h2 className="h2 features__heading">Processo</h2>
+            <p className="text-reg features__subheading">
+              Esses processos de análise de gases dissolvidos 
+              ou análise cromatográfica do óleo isolante são realizados a partir da amostra do óleo isolante de um 
+              transformador em operação, quantificando a concentração de certos gases gerados no equipamento e 
+              dissolvidos no óleo isolante, indicando a presença de falhas incipientes associadas à parte ativa do 
+              transformador.
+            </p>
+          </div>
         </div>
+        
         
         </div>
         <div className="divCollumn">
--- Diagnostico/src/components/Hero/Hero.jsx
+++ Diagnostico/src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Hero.css";
 import arrow from "../../assets/arrow.svg";
-import abstractShapes from "../../assets/abstract-shapes.png";
+import abstractShapes from "../../assets/Transformador.png";
 
 const Hero = () => {
   return (
@@ -14,7 +14,7 @@ const Hero = () => {
            de potência
         </h1>
         <p className="text-reg hero__subheading">
-          Diagnóstico inteligente de falhas em transformadores de potência com base em DGA, Análise de Gases Dissolvidos.
+          Diagnóstico inteligente de falhas em transformadores de potência com base em DGA, Análise de Gases Dissolvidos. Utilizando o modelo XGBoost, eXtreme Gradient Boosting é uma biblioteca de aprendizado de máquina distribuída e de código aberto que utiliza o algoritmo de árvores de decisão com aumento de gradiente. É conhecido por ser um algoritmo muito eficiente, rápido e preciso, tornando-se popular para resolver problemas de classificação e regressão. 
         </p>
       </div>
       <div className="hero__column">
